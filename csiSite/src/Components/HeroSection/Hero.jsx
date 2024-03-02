4a64ab2387e132a6be7c97beef7fc209af4524dc
M Sri Lalith <msrilalith30@gmail.com>
2024-03-02 20:46:21 +0530

    responsiveness fixed

--- csiSite/src/Components/HeroSection/Hero.jsx
+++ csiSite/src/Components/HeroSection/Hero.jsx
@@ -5,7 +5,7 @@ const Hero = ({mode}) => {
   return (
     <div className={`p-4 flex flex-col flex-wrap justify-center items-center gap-4 ${mode ? 'bg-[#D9EBFF] text-[#0F232A]' : 'text-[#C9DBEE] bg-[#112A46]'}`}>
         
-      <div className=" flex  flex-col justify-center items-center mb-4 md:mb-0 h-[300px] w-[400px]">
+      <div className="md:w-auto flex  flex-col justify-center items-center mb-4 md:mb-0 h-[300px] w-[400px]">
 
         <h1 className="text-xl font-bold md:text-5xl">COMPUTER SOCIETY OF INDIA</h1>
         <ReactTyped  className='text-2xl' strings={["SRMIST DELHI-NCR CAMPUS"]} typeSpeed={40} backSpeed={50} loop/>
