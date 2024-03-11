1b9d6825d0076a17bd99869f562bde0e1de8fd6d
M Sri Lalith <msrilalith30@gmail.com>
2024-03-11 19:54:13 +0530

    no events page

--- csiSite/src/Components/Forms/NoEvents.jsx
+++ csiSite/src/Components/Forms/NoEvents.jsx
@@ -1,9 +1,15 @@
-import React from 'react'
+import React from 'react';
 
 const NoEvents = () => {
   return (
-    <div className='w-full h-screen'>NoEvents</div>
-  )
-}
+    <div className="flex flex-col justify-center items-center min-h-screen  font-robboto">
+      <div className="text-center w-[fit] h-[120px] p-3 bg-[#9eacb744] border rounded-md text-neutral-900">
+        <span className="text-3xl">Sorry</span> <br />
+        Right now there are no ongoing events for you to register <br />
+        You'll be notified when to register!!!!
+      </div>
+    </div>
+  );
+};
 
-export default NoEvents
+export default NoEvents;
